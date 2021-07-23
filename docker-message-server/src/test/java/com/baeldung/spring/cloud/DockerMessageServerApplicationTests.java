cf4a70e58b6301285c08353d361b1d6f14faf50e
Rose Mogusu <rosemogusu@adalabsafrica.com>
2021-07-23 10:52:22 +0300

    Add initial commit

---
+++ docker-message-server/src/test/java/com/baeldung/spring/cloud/DockerMessageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.spring.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerMessageServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
